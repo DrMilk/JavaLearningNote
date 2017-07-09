dc65ab7a855954594ed05758d4bb4afa6b268a52
Administrator <18249028972@163.com>
2017-07-09 22:29:32 +0800

    Initial commit

---
+++ src/testres/Main.java
@@ -0,0 +1,18 @@
+package testres;
+
+import notecollection.*;
+import noteexception.WuException;
+import noteio.WuFile;
+import noteio.Wuio;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        WuException wuException=new WuException();
+        wuException.testException();
+    }
+
+}
+
