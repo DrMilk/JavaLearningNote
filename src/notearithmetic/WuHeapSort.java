81475524b370691b82ca6511c7f9898bad68a874
Administrator <18249028972@163.com>
2017-08-16 20:45:43 +0800

    堆排序

---
+++ src/notearithmetic/WuHeapSort.java
@@ -0,0 +1,8 @@
+package notearithmetic;
+
+/**
+ * Created by Administrator on 2017/8/16.
+ * 堆排序
+ */
+public class WuHeapSort {
+}
--- src/writtentest/NoteOne.java
+++ src/writtentest/NoteOne.java
@@ -1,8 +1,22 @@
 package writtentest;
 
+import java.util.ArrayList;
+
 /**
  * Created by Administrator on 2017/8/14.
  */
 public class NoteOne {
     // 二叉树  前序 ：左右根   中序 ： 左根右   后序 ： 跟左右
+
+    public static void main(String[] args){
+        int [] a = {1,3,5,7,9};
+        int [] b = {2,4,5,7,9};
+        for(int i = 0;i<a.length;i++){
+            if(a[i]==b[i]){
+                System.out.println(1);
+            }else {
+                System.out.println(0);
+            }
+        }
+    }
 }
